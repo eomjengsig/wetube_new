f71a660a376797515b96bcdbb42c48b3bafd7075
eomjengsig <travelereom@naver.com>
2022-10-10 02:37:42 +0900

    xczxccxz

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-eom",
+  acl: "public-read",
+});
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
@@ -27,8 +43,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: { fieldSize: 10000000 },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fieldSize: 10000000 },
+  storage: multerUploader,
 });
